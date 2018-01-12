81c26b7e446d27ea399cd6d9b2c49f17a0afbaae
Reuben Groenewald <reuben@xineoh.com>
2018-01-12 16:02:33 +0200

    jslint issues resolved

--- js/gmaps.js
+++ js/gmaps.js
@@ -4,6 +4,39 @@ var openStreetView;
 var toggleStreetView;
 var mapErrorHandler;
 
+// see https://developers.google.com/maps/documentation/javascript/markers -> Converting MarkerImage objects to type Icon
+// and SVG path notation https://www.w3.org/TR/SVG/paths.html#PathData
+// http://blog.mridey.com/2010/03/using-markerimage-in-maps-javascript.html
+function createMarkerCustomColor(icon_settings) {
+    return new google.maps.MarkerImage(
+        icon_settings.url,
+        new google.maps.Size(icon_settings.size.x, icon_settings.size.y),
+        new google.maps.Point(icon_settings.origin.x, icon_settings.origin.y),
+        new google.maps.Point(icon_settings.anchor.x, icon_settings.anchor.y),
+        new google.maps.Size(icon_settings.scaledSize.x, icon_settings.scaledSize.y)
+    );
+}
+
+// Adds a marker to the map.
+function addMarker(marker, indx) {
+    var location = {lat: marker.lat, lng: marker.lng};
+    var dynamic_icon = createMarkerCustomColor(marker.icon_settings);
+    return new google.maps.Marker({
+        position: location,
+        title: marker.caption,
+        icon: dynamic_icon,
+        map: gmap
+    });
+}
+
+// used to prevent redundancy
+function updateModalInfo (caption, excerpt, colour) {
+  AppViewModel.modal_caption(caption);
+  AppViewModel.modal_excerpt(excerpt);
+  AppViewModel.modal_text_colour("#" + colour);
+}
+
+
 function initMap() {
     // Used to check if google maps has been loaded
     // https://stackoverflow.com/questions/9228958/how-to-check-if-google-maps-api-is-loaded
@@ -14,39 +47,9 @@ function initMap() {
             center: {lat: 43.653226, lng: -79.383184},
             zoom: 11
         };
+
         // gmap actually initialized here
         gmap = new google.maps.Map(document.getElementById('map'), map_params);
-
-        // see https://developers.google.com/maps/documentation/javascript/markers -> Converting MarkerImage objects to type Icon
-        // and SVG path notation https://www.w3.org/TR/SVG/paths.html#PathData
-        // http://blog.mridey.com/2010/03/using-markerimage-in-maps-javascript.html
-        function createMarkerCustomColor(icon_settings) {
-            return new google.maps.MarkerImage(
-                icon_settings.url,
-                new google.maps.Size(icon_settings.size.x, icon_settings.size.y),
-                new google.maps.Point(icon_settings.origin.x, icon_settings.origin.y),
-                new google.maps.Point(icon_settings.anchor.x, icon_settings.anchor.y),
-                new google.maps.Size(icon_settings.scaledSize.x, icon_settings.scaledSize.y)
-            );
-        }
-
-        // Adds a marker to the map.
-        function addMarker(marker, indx) {
-            var location = {lat: marker.lat, lng: marker.lng};
-            var dynamic_icon = createMarkerCustomColor(marker.icon_settings);
-            return new google.maps.Marker({
-                position: location,
-                title: marker.caption,
-                icon: dynamic_icon,
-                map: gmap
-            });
-        }
-        // used to prevent redundancy
-        function updateModalInfo (caption, excerpt, colour) {
-          AppViewModel.modal_caption(caption);
-          AppViewModel.modal_excerpt(excerpt);
-          AppViewModel.modal_text_colour("#" + colour);
-        }
 
         // setup markers by looping through locations
         AppViewModel.locations.forEach(function(marker_info, indx) {
@@ -89,13 +92,13 @@ function initMap() {
         // set visibility of street view
         toggleStreetView = function (visible) {
           if (panorama) {
-            if (visible == true) {
+            if (visible === true) {
               panorama.setVisible(true);
             } else {
               panorama.setVisible(false);
             }
           }
-        }
+        };
         // setup street view and show
         openStreetView = function (location) {
           panorama = gmap.getStreetView();
@@ -105,10 +108,10 @@ function initMap() {
             pitch: 0
           }));
           toggleStreetView(true);
-        }
+        };
     }
 }
 
 mapErrorHandler = function() {
   AppViewModel.mapLoaded(false);
-}
+};
